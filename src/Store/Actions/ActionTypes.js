29b30d88ea5f0e0435e9c357801a8a3bf3b98b38
Abdessamad BAHHOUS <abdo.bahhous@gmail.com>
2020-12-06 00:47:40 +0100

    Prepare reducer and action types

--- src/Store/Actions/ActionTypes.js
+++ src/Store/Actions/ActionTypes.js
@@ -1,2 +1,3 @@
 // New actions
 export const LOAD_NEWS_LIST = "LOAD_NEWS_LIST";
+export const LOAD_TOP_NEWS_LIST = "LOAD_TOP_NEWS_LIST";
--- src/Store/Reducers/Main.js
+++ src/Store/Reducers/Main.js
@@ -3,6 +3,8 @@ import { updateObject } from "../../Helpers/Functions";
 
 const initState = {
   newsList: false,
+  topNewsList: false,
+  customNewsList: false,
 };
 
 const MainReducer = (state = initState, action) => {
@@ -10,6 +12,14 @@ const MainReducer = (state = initState, action) => {
     case actionTypes.LOAD_NEWS_LIST:
       return updateObject(state, {
         newsList: action.newsList,
+      });
+    case actionTypes.LOAD_TOP_NEWS_LIST:
+      return updateObject(state, {
+        topNewsList: action.topNewsList,
+      });
+    case actionTypes.LOAD_TOP_NEWS_LIST:
+      return updateObject(state, {
+        customNewsList: action.customNewsList,
       });
     default:
       return state;
