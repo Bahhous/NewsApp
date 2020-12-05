e37dc9b245c5facd3514694a12a2aad700364658
Abdessamad BAHHOUS <abdo.bahhous@gmail.com>
2020-12-05 16:53:10 +0100

    Add error boundary

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import {
   BrowserRouter as Router,
   Switch,
 } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 import { PUBLIC_ROUTE } from "./route.constants";
 
 import Loader from "./Components/Loader";
@@ -19,21 +20,23 @@ const publicRoutes = [
 
 export default function Routes() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Router>
-        <Switch>
-          {publicRoutes.map((route, index) => (
-            <Route key={index} path={route.path} exact={route.exact}>
-              <route.component />
-            </Route>
-          ))}
-          <Redirect
-            to={{
-              pathname: PUBLIC_ROUTE.LANDING,
-            }}
-          />
-        </Switch>
-      </Router>
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<Loader />}>
+        <Router>
+          <Switch>
+            {publicRoutes.map((route, index) => (
+              <Route key={index} path={route.path} exact={route.exact}>
+                <route.component />
+              </Route>
+            ))}
+            <Redirect
+              to={{
+                pathname: PUBLIC_ROUTE.LANDING,
+              }}
+            />
+          </Switch>
+        </Router>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
