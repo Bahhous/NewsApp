9f9527876ea52c78b4f6a72788a0b4849b720f83
Abdessamad BAHHOUS <abdo.bahhous@gmail.com>
2020-12-05 16:27:27 +0100

    Create redux logger

---
+++ src/Store/Middlewares/Logger.js
@@ -0,0 +1,14 @@
+const Logger = (store) => {
+  return (next) => {
+    return (action) => {
+      if (window.location.hostname === "localhost")
+        console.log(
+          "[Midlewear] Dsipatching -> " + action.type + " => ",
+          action
+        );
+      const res = next(action);
+      return res;
+    };
+  };
+};
+export default Logger;
